683af0bb897de105d7212d85caf637df4072e2b9
Loussaief-Mariem <lmariem2004@gmail.com>
2025-09-11 11:59:53 +0100

    Commit new version

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-stage-xi.vercel.app/api",
+  baseURL: "https://backend-stage-xi.vercel.app/api/",
 });
 //import.meta.env.VITE_API_URL,
 // // import.meta.env.VITE_API_URL,
